3d7c3f5e3d94b2127059ff8c78b61fb7d17e1d28
FloMoit <kororo@free.fr>
2024-05-02 10:26:22 +0200

    Fixed component call for vercel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MyForm } from "../components/MyForm.js";
+import { MyForm } from "../components/MyForm.tsx";
 import { NavLink } from "react-router-dom";
 import UsersTable from "../components/UsersTable.tsx";
 
