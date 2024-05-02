f8def29d14d1deb19554777b2f30b0d8bb85e02b
FloMoit <kororo@free.fr>
2024-05-02 20:37:55 +0200

    Added Header to the users table

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,10 +1,21 @@
+import { NavLink } from "react-router-dom";
 import { UsersTable } from "../components/UsersTable";
 
 export default function Home() {
   return (
     <>
       <div className="mx-16 my-6 ">
-        <UsersTable />
+        <div className="flex justify-center gap-6 mx-0 my-6 text-xl font-semibold">
+          <NavLink
+            to="/"
+            className="underline text-slate-200 underline-offset-4">
+            Create User
+          </NavLink>
+          <h2>View Current Users</h2>
+        </div>
+        <div className="flex justify-center">
+          <UsersTable />
+        </div>
       </div>
     </>
   );
